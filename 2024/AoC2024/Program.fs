5f9c95ede5947e86b1ef9ee5529263c463c18bed
Johan Olsson <johan.olsson@non.se.com>
2024-12-11 15:37:59 +0100

    Day10

--- 2024/AoC2024/Program.fs
+++ 2024/AoC2024/Program.fs
@@ -1,6 +1,6 @@
 ﻿
 [<EntryPoint>]
 let main argv = 
-    day09.secondPart day09.day09Input
+    day10.secondPart day10.day10Input
     |> printfn "%A" 
     0 // return an integer exit code
---
+++ 2024/AoC2024/day10.fs
@@ -0,0 +1,44 @@
+﻿module day10
+
+open System.IO
+
+type Point = { X: int; Y: int; Z: int }
+
+let day10Input = 
+    File.ReadAllLines "./input/day10.txt"
+    |> Array.map (fun row -> row.ToCharArray() |> Array.map (fun c -> int c - int '0'))
+
+let toPoints (input : int array array) =
+    [ for y = 0 to input.Length - 1 do
+        for x = 0 to input.[y].Length - 1 do
+            yield { X = x; Y = y; Z = input[x][y] }]
+
+let checkPoints pos height =
+    [ { pos with X = pos.X + 1; Z = height }
+      { pos with X = pos.X - 1; Z = height }
+      { pos with Y = pos.Y + 1; Z = height }
+      { pos with Y = pos.Y - 1; Z = height }]
+
+let scoreTrail head map scoreFunc =
+    let rec findTrailTails curHeight pos =
+        match curHeight with
+        | 9 -> [pos]
+        | _ -> 
+            checkPoints pos (curHeight + 1)
+            |> List.filter (fun p -> map |> List.contains p)
+            |> List.collect (fun p -> findTrailTails (curHeight + 1) p)
+
+    findTrailTails 0 head |> scoreFunc
+
+let findTrails scoreFunc input =
+    let map = input |> toPoints
+    map 
+    |> List.filter (fun p -> p.Z = 0)
+    |> List.map (fun p -> scoreTrail p map scoreFunc)
+    |> List.sum
+
+let firstPart input =
+    input |> findTrails (List.distinct >> List.length)
+
+let secondPart input =
+    input |> findTrails List.length
